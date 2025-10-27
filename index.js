4ae751f888a1ea490be93cc233f660e960266b44
L3VI04 <ljenkins@anamosa.k12.ia.us>
2025-10-27 13:16:37 -0500

    Update seasons array and modify HTML content

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+// Create a variable named seasons containing the four seasons
+const seasons = ['Summer', 'Fall', 'Winter', 'Spring'];
+
+// Update the second item (index 1) to be 'Autumn'
+seasons[1] = 'Autumn';
+
+// Assign the innerHTML of the element with id "result" to the second element in the seasons array
+document.getElementById('result').innerHTML = seasons[1];
 
